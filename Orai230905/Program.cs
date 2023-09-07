326982750d83b2e5f654de9e336104d8458cad00
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-07 09:50:51 +0200

    Add files via upload

---
+++ Orai230905/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Orai230905
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
