28dcb234ca33cc6972fcca432615240200e8f160
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-07 10:26:32 +0200

    Update Program.cs

--- Orai230905/Program.cs
+++ Orai230905/Program.cs
@@ -6,7 +6,19 @@ namespace Orai230905
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var kategoriak = new List<Kategoria>();
+            var sr = new StreamReader(
+                path: @"..\..\..\src\titanic.txt",
+                encoding: Encoding.UTF8);
+
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine();
+                var kat = new Kategoria(sor);
+                kategoriak.Add(kat);
+            }
+
+            Console.WriteLine($"2. feladat: {kategoriak.Count} db");
         }
     }
 }
