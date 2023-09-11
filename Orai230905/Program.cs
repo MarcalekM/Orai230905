3908be761f03a0218af4a5f3b8a4f1df0f72003a
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-11 09:11:19 +0200

    3. feladat

--- Orai230905/Program.cs
+++ Orai230905/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
 
 namespace Orai230905
 {
@@ -13,12 +17,21 @@ namespace Orai230905
 
             while (!sr.EndOfStream)
             {
-                string sor = sr.ReadLine();
-                var kat = new Kategoria(sor);
-                kategoriak.Add(kat);
+                //string sor = sr.ReadLine();
+                //var kat = new Kategoria(sor);
+                //kategoriak.Add(kat);
+                kategoriak.Add(new Kategoria(sr.ReadLine()));
             }
 
             Console.WriteLine($"2. feladat: {kategoriak.Count} db");
+
+            //var f3 = kategoriak.Sum(k => k.UtasokSzama);
+            int f3 = 0;
+            foreach(Kategoria k in kategoriak)
+            {
+                f3 += k.UtasokSzama;
+            }
+            Console.WriteLine($"3. feladat: {f3} fő");
         }
     }
 }
