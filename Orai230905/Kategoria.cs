3349462074509a49f5e7affcf8824ba80630434a
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-11 09:06:37 +0200

    get UtasokSzama

--- Orai230905/Kategoria.cs
+++ Orai230905/Kategoria.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,13 +9,15 @@ namespace Orai230905
         public string KategoriaNev { get; set; }
         public int TulelokSzama { get; set; }
         public int EltuntekSzama { get; set; }
+        public int UtasokSzama => EltuntekSzama + TulelokSzama;
+        
     
         public Kategoria(string beolvasottSor)
         {
             var darabok = beolvasottSor.Split(';');
-            this.KategoriaNev = darabok[0];
-            this.TulelokSzama = int.Parse(darabok[1]);
-            this.EltuntekSzama = int.Parse(darabok[2]);
+            KategoriaNev = darabok[0];
+            TulelokSzama = int.Parse(darabok[1]);
+            EltuntekSzama = int.Parse(darabok[2]);
         }
     
     }
